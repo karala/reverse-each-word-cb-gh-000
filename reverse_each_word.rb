fef1504d0f0336d565e479de054bdcca7c8d0d5c
Shubham Karala <shubham@ramanujan.du.ac.in>
2019-10-25 05:27:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   sentence.split.each do |words|
     reverse_words = words
-    return reverse_words.reverse.join(" ")
+    return reverse_words.reverse
   end
 end
